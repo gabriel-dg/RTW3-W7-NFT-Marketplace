fb9ed4f1574ff347df37be1ce208e9c744ed87d0
gabriel-dg <gabrieldg@gmail.com>
2022-06-15 18:04:42 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  // etherscan: {
+  //   apiKey: {
+  //     goerli: process.env.GOERLI_API_KEY
+  //   }
+  // }
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
